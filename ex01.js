a7d60f1dc55f432ceb6c24f3467d7b4ff9571417
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-05-30 21:28:10 -0300

    resolucao do exercicio 01

--- ex01.js
+++ ex01.js
@@ -1,2 +1,26 @@
 const prompt = require("prompt-sync")();
 console.clear();
+
+let arrayNumeros = [];
+let numsPares = [];
+let numImpares = [];
+
+let contador = 0;
+
+while (contador < 20) {
+  let novoNum = +prompt("Digite um número: ");
+  arrayNumeros.push(novoNum);
+  contador++;
+}
+
+for (let i = 0; i < arrayNumeros.length; i++) {
+  if (arrayNumeros[i] % 2 == 0) {
+    numsPares.push(arrayNumeros[i]);
+  } else {
+    numImpares.push(arrayNumeros[i]);
+  }
+}
+
+console.log("Array todo: ", arrayNumeros);
+console.log("Numeros pares: ", numsPares);
+console.log("Numeros Impares: ", numImpares);
